6f2dbd8032c766b568f1cbd5578d9ffe82a3dbd5
Chan Lee <chanho0325@gmail.com>
2025-03-23 21:05:24 +0900

    Update package configuration and add initial implementation for NestJS DTO generator

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander';
+import { version } from '../package.json';
+
+const program = new Command();
+
+program
+  .name('nest-dto-generator')
+  .description('NestJS DTO 생성기')
+  .version(version);
+
+program.parse();
+
 console.log('Happy developing ✨');
